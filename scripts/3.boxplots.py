0f6d746e934fc725a024b915d95552993e60cce2
rob <robvanderg@live.nl>
2024-07-17 10:44:12 +0200

    Subtract values to incorporate the baseline in a cleaner way

--- scripts/3.boxplots.py
+++ scripts/3.boxplots.py
@@ -4,7 +4,7 @@ mpl.use('Agg')
 import matplotlib.pyplot as plt
 import ast
 plt.style.use('scripts/rob.mplstyle')
-
+import pprint
 
 def dataset2boxplot(dataset, setting):
     print(dataset + '-' + setting)
@@ -43,49 +43,33 @@ def dataset2boxplot(dataset, setting):
     fig, ax = plt.subplots(figsize=(8,5), dpi=300)
     colors = plt.rcParams["axes.prop_cycle"].by_key()["color"]
     colors = colors+colors
-    for methodIdx, method in enumerate(myutils.settings + ['expected']):
+    for methodIdx, method in enumerate(myutils.settings):
         if method not in boxplots:
             continue
         # transpose
         data = list(map(list, zip(*boxplots[method])))
-        if method != 'expected':
-            baseline_data = list(map(list, zip(*baseline_boxplots[method])))
-        # remove empty items
+        baseline_data = list(map(list, zip(*baseline_boxplots[method])))
+        # subtract
         for i in range(len(data)):
             for j in reversed(range(len(data[i]))):
                 if data[i][j] == '--':
                     del data[i][j]
                 else:
-                    data[i][j] = float(data[i][j])
-        if method != 'expected':
-            for i in range(len(baseline_data)):
-                for j in reversed(range(len(baseline_data[i]))):
-                    if baseline_data[i][j] == '--':
-                        del baseline_data[i][j]
-                    else:
-                        baseline_data[i][j] = float(baseline_data[i][j])
-
+                    print(data[i][j], baseline_data[i][j])
+                    data[i][j] = float(data[i][j])-float(baseline_data[i][j])
+        print(data)
         positions = [.12+i + methodIdx * .12 for i in range(len(data))]
         bp = ax.boxplot(data, positions=positions, patch_artist=True, widths=.12, label=method)
-        if method != 'expected':
-            bp2 = ax.boxplot(baseline_data, positions=positions, patch_artist=True, widths=.06)
-
         edge_color = 'black'
         fill_color = colors[methodIdx]
 
         for element in ['boxes', 'whiskers', 'fliers', 'means', 'medians', 'caps']:
             plt.setp(bp[element], color=edge_color)
-            if method != 'expected':
-                plt.setp(bp2[element], color=edge_color)
         for patch, color in zip(bp['boxes'], colors):
             patch.set_facecolor(fill_color)
-        if method != 'expected':
-            for patch, color in zip(bp2['boxes'], colors):
-                patch.set_facecolor(fill_color)
         plt.xticks(range(6))
         ax.set_xticklabels(['' for _ in range(6)])
 
-    
     ax.set_xlim((0,5))
     leg = ax.legend()
     leg.get_frame().set_linewidth(1.5)
